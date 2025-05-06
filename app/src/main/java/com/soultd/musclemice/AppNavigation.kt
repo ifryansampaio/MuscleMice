8404094a64b3b62ae2539a2ed969e951d12e205e
Ryan Sampaio <rssdo.dev@gmail.com>
2025-05-06 00:06:58 -0300

    Update main/navigation

--- app/src/main/java/com/soultd/musclemice/AppNavigation.kt
+++ app/src/main/java/com/soultd/musclemice/AppNavigation.kt
@@ -4,17 +4,16 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun AppNavigation(navController: NavHostController) {
+fun AppNavigation() {
+    val navController = rememberNavController()
+
     NavHost(navController = navController, startDestination = "splash") {
         composable("splash") { SplashScreen(navController) }
         composable("login") { LoginScreen(navController) }
         composable("register") { RegisterScreen(navController) }
-        //composable("home") { HomeScreen(navController) }
-        //composable("start_workout") { StartWorkoutScreen(navController) }
-        //composable("series_timer") { SeriesTimerScreen(navController) }
-
-        //composable("settings") { SettingsScreen(navController) }
+        // composable("home") { HomeScreen(navController) }
     }
 }
--- app/src/main/java/com/soultd/musclemice/MainActivity.kt
+++ app/src/main/java/com/soultd/musclemice/MainActivity.kt
@@ -6,54 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.navigation.compose.rememberNavController
 import com.soultd.musclemice.ui.theme.MuscleMiceTheme
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MuscleMiceTheme {  //Aplica o tema a UI
-
-                class MainActivity : ComponentActivity() {
-                    override fun onCreate(savedInstanceState: Bundle?) {
-                        super.onCreate(savedInstanceState)
-                        setContent {
-                            MuscleMiceTheme {
-                                val navController = rememberNavController()
-                                Surface(color = MaterialTheme.colors.background) {
-                                    AppNavigation(navController)
-                                }
-                            }
-                        }
-                    }
+            MuscleMiceTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    AppNavigation()
                 }
-
-//                val navController = rememberNavController() //Cria e lembra um NavControl
-//
-//                NavHost(navController = navController, startDestination = "splash") {
-//
-//                    composable(route = "splash") { SplashScreen(navController = navController) }
-//
-//                    composable("login") {
-//                        LoginScreen(
-//                            onLoginClick = { email, senha ->
-//
-//                                // Se a verificação for bem-sucedido, navega pra tela principal:
-//                                navController.navigate("home") {
-//                                    popUpTo("login") { inclusive = true }
-//                                }
-//                            },
-//                            onNavigateToRegister = {
-//                                navController.navigate("register")
-//                            }
-//                        )
-//                    }
-//                }
             }
         }
     }
